c5fff98afdf39e4cd233bf5c466cc34a86b5ebbe
pablog8 <pablog@usal.es>
2018-09-26 01:34:30 +0200

    Actualización

--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -1,16 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sales.ViewModels
+﻿namespace Sales.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Sales.Views;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+
     public class MainViewModel
     {
         public ProductsViewModel Products { get; set; }
 
+        public AddProductViewModel AddProduct { get; set; }
+
         public MainViewModel()
         {
             this.Products = new ProductsViewModel();
+        }
+        //cuando toque en el icono +, lanza el comando AddProductComand, que devuelve la pagina del metodo GoToAddProduct
+        public ICommand AddProductCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToAddProduct);
+            }
+        }
+
+        private async void GoToAddProduct()
+        {
+            //Antes de lanzar la pagina instanciamos la viewmodel que gobierna la pagina
+            this.AddProduct = new AddProductViewModel();
+
+            await Application.Current.MainPage.Navigation.PushAsync(new AddProductPage());
         }
     }
 }
