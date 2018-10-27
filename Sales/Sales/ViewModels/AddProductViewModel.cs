df512cbe2ffb759687b1ecd5c94e38196eec8786
pablog8 <pablog@usal.es>
2018-10-27 21:38:06 +0200

    categorias

--- Sales/Sales/ViewModels/AddProductViewModel.cs
+++ Sales/Sales/ViewModels/AddProductViewModel.cs
@@ -10,6 +10,9 @@
     using Plugin.Media.Abstractions;
     using System;
     using Plugin.Media;
+    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     public class AddProductViewModel : BaseViewModel
     {
@@ -20,9 +23,29 @@
         private APIService apiService;
         private bool isRunning;
         private bool isEnabled;
+        private ObservableCollection<Category> categories;
+        private Category category;
         #endregion
 
         #region Properties
+
+        public List<Category> MyCategories { get; set; }
+
+        //cuando seleccionamos una categoria el selected item
+        public Category Category {
+
+            get { return this.category; }
+            set { this.SetValue(ref this.category, value); }
+
+        }
+
+        //la que bindamos al itemsource
+        public ObservableCollection<Category> Categories
+        {
+            get { return this.categories; }
+            set { this.SetValue(ref this.categories, value); }
+        }
+
         public string Description { get; set; }
 
         public string Price { get; set; }
@@ -53,8 +76,57 @@
             this.apiService = new APIService();
             this.IsEnabled = true;
             this.ImageSource = "noproduct";
+            this.LoadCategories();
         }
         #endregion
+
+        #region Methods
+        private async void LoadCategories()
+        {
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, connection.Message, Languages.Accept);
+                return;
+            }
+
+            var answer = await this.LoadCategoriesFromAPI();
+            if (answer)
+            {
+                this.RefreshList();
+                
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+        }
+
+        private void RefreshList()
+        {
+            this.Categories = new ObservableCollection<Category>(this.MyCategories.OrderBy(c => c.Description));
+        }
+
+        private async Task<bool> LoadCategoriesFromAPI()
+        {
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlCategoriesController"].ToString();
+            var response = await this.apiService.GetList<Category>(url, prefix, controller, Settings.TokenType, Settings.AccessToken);
+            if (!response.IsSuccess)
+            {
+                return false;
+            }
+
+            this.MyCategories = (List<Category>)response.Result;
+            return true;
+        }
+        #endregion
+
 
         #region Commands
 
@@ -152,6 +224,16 @@
                     Languages.Accept);
                 return;
             }
+
+            if (this.Category == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.CategoryError,
+                    Languages.Accept);
+                return;
+            }
+
             this.IsRunning = true;
             this.IsEnabled = false;
 
@@ -182,7 +264,10 @@
                 Price = price,
                 Remarks = this.Remarks,
                 ImageArray = imageArray,
+                CategoryId = this.Category.CategoryId,
+                UserId = MainViewModel.GetInstance().UserASP.Id,
             };
+
 
             var url = Application.Current.Resources["UrlAPI"].ToString();
 
