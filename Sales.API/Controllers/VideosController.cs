00c43ea63e188640752742821296d9bd7abd68db
pablog8 <pablog@usal.es>
2018-11-14 19:23:35 +0100

    public api videos

---
+++ Sales.API/Controllers/VideosController.cs
@@ -0,0 +1,19 @@
+﻿namespace Sales.API.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using Common.Models;
+    using Domain.Models;
+
+    [Authorize]
+    public class VideosController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        public IQueryable<Video> GetVideos()
+        {
+            return db.Videos.OrderBy(c => c.Description);
+        }
+    }
+
+}
