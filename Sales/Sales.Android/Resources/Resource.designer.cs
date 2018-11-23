574c6eff660f704d683ce4c7944b4ea03e3d0ebb
pablog8 <pablog@usal.es>
2018-11-23 15:16:27 +0100

    usuario lesion negro

--- Sales/Sales.Android/Resources/Resource.designer.cs
+++ Sales/Sales.Android/Resources/Resource.designer.cs
@@ -2407,26 +2407,26 @@ namespace Sales.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020146
-			public const int avd_hide_password_1 = 2130837830;
-			
-			// aapt resource value: 0x7f020147
-			public const int avd_hide_password_2 = 2130837831;
-			
 			// aapt resource value: 0x7f020148
-			public const int avd_hide_password_3 = 2130837832;
+			public const int avd_hide_password_1 = 2130837832;
+			
+			// aapt resource value: 0x7f020149
+			public const int avd_hide_password_2 = 2130837833;
+			
+			// aapt resource value: 0x7f02014a
+			public const int avd_hide_password_3 = 2130837834;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020149
-			public const int avd_show_password_1 = 2130837833;
-			
-			// aapt resource value: 0x7f02014a
-			public const int avd_show_password_2 = 2130837834;
-			
 			// aapt resource value: 0x7f02014b
-			public const int avd_show_password_3 = 2130837835;
+			public const int avd_show_password_1 = 2130837835;
+			
+			// aapt resource value: 0x7f02014c
+			public const int avd_show_password_2 = 2130837836;
+			
+			// aapt resource value: 0x7f02014d
+			public const int avd_show_password_3 = 2130837837;
 			
 			// aapt resource value: 0x7f020055
 			public const int common_full_open_on_phone = 2130837589;
@@ -3103,11 +3103,11 @@ namespace Sales.Droid
 			// aapt resource value: 0x7f020135
 			public const int notification_icon_background = 2130837813;
 			
-			// aapt resource value: 0x7f020144
-			public const int notification_template_icon_bg = 2130837828;
+			// aapt resource value: 0x7f020146
+			public const int notification_template_icon_bg = 2130837830;
 			
-			// aapt resource value: 0x7f020145
-			public const int notification_template_icon_low_bg = 2130837829;
+			// aapt resource value: 0x7f020147
+			public const int notification_template_icon_low_bg = 2130837831;
 			
 			// aapt resource value: 0x7f020136
 			public const int notification_tile_bg = 2130837814;
@@ -3140,16 +3140,22 @@ namespace Sales.Droid
 			public const int usercirculo = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int usuario = 2130837824;
+			public const int userlesion = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int usuariocirculo = 2130837825;
+			public const int usuario = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int videoicon = 2130837826;
+			public const int usuariocirculo = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int videos = 2130837827;
+			public const int usuariolesion = 2130837827;
+			
+			// aapt resource value: 0x7f020144
+			public const int videoicon = 2130837828;
+			
+			// aapt resource value: 0x7f020145
+			public const int videos = 2130837829;
 			
 			static Drawable()
 			{
